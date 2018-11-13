84e05d22643f7050cc430736872f1dfbf733a4cd
VsevolodKurochka <dramdoobas123456@gmail.com>
2018-11-13 16:50:52 +0200

    Final custom mobile phone value

--- app/assets/src/js/babel/jquery.main.babel.js
+++ app/assets/src/js/babel/jquery.main.babel.js
@@ -1,6 +1,15 @@
 $(document).ready(function(){
 	$('#landing-form').submit(function(e){
-		e.preventDefault();
-		
+		//e.preventDefault();
+		var form = $(this);
+		var phoneVal = form.find('#phone').val();
+
+		var custom_mobile_phone = "input[name='custom_mobile_phone']";
+		var custom_mobile_phone_value = phoneVal.replace(' ', '');
+		custom_mobile_phone_value = custom_mobile_phone_value.replace('-', '');
+		custom_mobile_phone_value = custom_mobile_phone_value.replace('(', '');
+		custom_mobile_phone_value = custom_mobile_phone_value.replace(')', '');
+		custom_mobile_phone_value = custom_mobile_phone_value.replace('-', '');
+		custom_mobile_phone_value = custom_mobile_phone_value.replace(' ', '');
 	});
 });
